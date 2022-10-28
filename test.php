8067230551811bac9773950f7ac98c0e5280cb25
AbduvakhobAkhmedov <116796617+AbduvakhobAkhmedov@users.noreply.github.com>
2022-10-28 12:12:34 +0300

    Add files via upload

---
+++ test.php
@@ -0,0 +1,30 @@
+<?php
+$name = "не определено";
+$age = "не определен";
+if(isset($_POST["name"])){
+ 
+    $name = $_POST["name"];
+}
+if(isset($_POST["age"])){
+ 
+    $age = $_POST["age"];
+}
+echo "Имя: $name <br> Возраст: $age";
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>METANIT.COM</title>
+<meta charset="utf-8" />
+</head>
+<body>
+<h3>Форма ввода данных</h3>
+<form action="user.php" method="POST">
+    <p>Имя: <input type="text" name="name" /></p>
+    <p>Возраст: <input type="number" name="age" /></p>
+    <input type="submit" value="Отправить">
+</form>
+</body>
+</html>
